43bded3bd11c2faec3bd984d2f82aab461eff2cd
JOSE CARLOS BARRETO FILHO <85513751+JCARLOS3009@users.noreply.github.com>
2024-08-21 23:27:16 -0300

    Update ctable.py

--- ctable.py
+++ ctable.py
@@ -1,4 +1,5 @@
 import psycopg2
+#conexão com o banco de dados
 conn = psycopg2.connect(database = "postgres", user = "postgres", password = "senha123", host = "127.0.0.1", port = "5432")
 print("Conexão com o Banco de Dados aberta com sucesso!")
 cur = conn.cursor()
@@ -6,3 +7,9 @@ cur.execute('''CREATE TABLE Agenda(ID INT PRIMARY KEY NOT NULL,Nome TEXT NOT NUL
 print("Tabela criada com sucesso!")
 conn.commit()
 conn.close()
+#Inserção de dados na tabela
+print ("Conexão com o Banco de Dados aberta com sucesso!") 
+cur=conn.cursor() 
+cur.execute("""INSERT INTO public."AGENDA" ("id", "nome" , "telefone" ) VALUES (1, 'Pessoa 1' , '02199999999' )""") 
+conn.commit() 
+print("Inserção realizada com sucesso!"); 8conn.close()
