be53bb2e1c084d7284b2b1cdfa4dfed4328bb573
JOSE CARLOS BARRETO FILHO <85513751+JCARLOS3009@users.noreply.github.com>
2024-08-21 23:28:53 -0300

    Update ctable.py

--- ctable.py
+++ ctable.py
@@ -8,8 +8,11 @@ print("Tabela criada com sucesso!")
 conn.commit()
 conn.close()
 #Inserção de dados na tabela
-print ("Conexão com o Banco de Dados aberta com sucesso!") 
-cur=conn.cursor() 
 cur.execute("""INSERT INTO public."AGENDA" ("id", "nome" , "telefone" ) VALUES (1, 'Pessoa 1' , '02199999999' )""") 
 conn.commit() 
 print("Inserção realizada com sucesso!"); 8conn.close()
+#Seleção dos dadso inseridos
+cur.execute("""select * from public."AGENDA" where "id"=1""") 
+registro=cur.fetchone() 
+print(registro) 8 conn.commit() 9 print("Seleção realizada com sucesso!"); 
+conn.close()
